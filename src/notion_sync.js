06e0cd8ef498715084487a8fe6ad70905438778a
Dorad <ddxid@outlook.com>
2025-07-30 21:43:54 +0800

    remove vendor path.

--- src/notion_sync.js
+++ src/notion_sync.js
@@ -69,18 +69,16 @@ let config = {
   timezone: core.getInput("timezone") || "",
 };
 
-// add current running file dir to PATH
-process.env.PATH = __dirname + ":" + process.env.PATH;
-// add all the exec file under __dirname/vendor* dirs the executable permission expect the source dir
-const { execSync } = require("child_process");
-const { url } = require("inspector");
-const { BADFAMILY } = require("dns");
-// try to find all the files under __dirname/vendor* dirs and set the executable permission
-try {
-  execSync(`find ${__dirname}/vendor* -type f -not -name "*.tar.gz" -exec chmod +x {} \\;`);
-} catch (e) {
-  core.error(`Failed to set the executable permission for all the files under ${__dirname}/vendor* dirs, error: ${e}`);
-}
+// // add current running file dir to PATH
+// process.env.PATH = __dirname + ":" + process.env.PATH;
+// // add all the exec file under __dirname/vendor* dirs the executable permission expect the source dir
+// const { execSync } = require("child_process");
+// // try to find all the files under __dirname/vendor* dirs and set the executable permission
+// try {
+//   execSync(`find ${__dirname}/vendor* -type f -not -name "*.tar.gz" -exec chmod +x {} \\;`);
+// } catch (e) {
+//   core.error(`Failed to set the executable permission for all the files under ${__dirname}/vendor* dirs, error: ${e}`);
+// }
 
 (async function () {
   core.startGroup('Notion2markdown-action')
